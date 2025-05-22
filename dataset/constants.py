7470f7c5870121d65965f30f22cc44bc160a249c
rleygonie <rebecca.leygonie@intradef.gouv.fr>
2025-05-22 15:29:37 +0000

    training ready

--- dataset/constants.py
+++ dataset/constants.py
@@ -1,33 +1,17 @@
 BASE_PATH = ""
 DATA_PATH = {
-    "Colon_clinicDB": f"{BASE_PATH}/data/Colon/CVC-ClinicDB",
-    "Colon_colonDB": f"{BASE_PATH}/data/Colon/CVC-ColonDB",
-    "Colon_cvc300": f"{BASE_PATH}/data/Colon/CVC-300",
-    "Colon_Kvasir": f"{BASE_PATH}/data/Colon/Kvasir",
+    "Colon_Kvasir": f"{BASE_PATH}./data/Colon/Kvasir",
 }
 
 
 CLASS_NAMES = {
-    "Colon_clinicDB": ["Colon_clinicDB"],
-    "Colon_colonDB": ["Colon_colonDB"],
     "Colon_Kvasir": ["Kvasir"],
-    "Colon_cvc300": ["CVC-300"],
 }
 
 DOMAINS = {
-    "Colon_clinicDB": "Medical",
-    "Colon_colonDB": "Medical",
     "Colon_Kvasir": "Medical",
-    "Colon_cvc300": "Medical",
 }
 REAL_NAMES = {
-    "Colon_clinicDB": {
-        "Colon_clinicDB": "colon endoscopy image",
-    },
-    "Colon_colonDB": {
-        "Colon_colonDB": "colon endoscopy image",
-    },
-    "Colon_cvc300": {"CVC-300": "colon endoscopy image"},
     "Colon_Kvasir": {"Kvasir": "colon endoscopy image"},
 }
 PROMPTS = {
